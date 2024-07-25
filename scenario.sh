e43f2e3ee3284546b69d1dc5b5e20e62da494ec7
pashakostohrys <pavel@codefresh.io>
2024-07-25 17:55:51 +0300

    gb2

--- scenario.sh
+++ scenario.sh
@@ -1,12 +1,12 @@
 for VARIABLE in 1 .. 2
 do
-   ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
+   # ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
    ./updater.sh ./apps/guestbook2/deployment.yaml "0.1" "0.2"
    ./updater.sh ./apps/guestbook3/deployment.yaml "0.1" "0.2"
 
    sleep 15
 
-   ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
+   # ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
    ./updater.sh ./apps/guestbook2/deployment.yaml "0.2" "0.1"
    ./updater.sh ./apps/guestbook3/deployment.yaml "0.2" "0.1"
 
