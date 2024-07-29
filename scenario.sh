9eb6edd5dae5160ed1c1ca33ebb53befbe2e2bc2
pashakostohrys <pavel@codefresh.io>
2024-07-29 14:07:40 +0300

    enable self heal

--- scenario.sh
+++ scenario.sh
@@ -1,16 +1,16 @@
 for VARIABLE in 1 .. 2
 do
-   # ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
+   ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
    ./updater.sh ./apps/guestbook2/deployment.yaml "0.1" "0.2"
    ./updater.sh ./apps/guestbook3/deployment.yaml "0.1" "0.2"
 
-   sleep 15
+   sleep 30
 
-   # ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
+   ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
    ./updater.sh ./apps/guestbook2/deployment.yaml "0.2" "0.1"
    ./updater.sh ./apps/guestbook3/deployment.yaml "0.2" "0.1"
 
-   sleep 15
+   sleep 30
 done
 
 
