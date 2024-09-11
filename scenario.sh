09d98f4de43288a8300d0f68824be82db9dbbfc9
pashakostohrys <pavel@codefresh.io>
2024-09-11 15:37:00 +0300

    gb1.1

--- scenario.sh
+++ scenario.sh
@@ -1,18 +1,33 @@
-for VARIABLE in 1 .. 2
+#for VARIABLE in 1 .. 2
+#do
+#   ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
+#   ./updater.sh ./apps/guestbook2/deployment.yaml "0.1" "0.2"
+#   ./updater.sh ./apps/guestbook3/deployment.yaml "0.1" "0.2"
+#
+#   sleep 30
+#
+#   ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
+#   ./updater.sh ./apps/guestbook2/deployment.yaml "0.2" "0.1"
+#   ./updater.sh ./apps/guestbook3/deployment.yaml "0.2" "0.1"
+#
+#   sleep 30
+#done
+#
+
+for VARIABLE in 1 .. 50
 do
-   ./updater.sh ./apps/guestbook/deployment.yaml "0.1" "0.2"
-   ./updater.sh ./apps/guestbook2/deployment.yaml "0.1" "0.2"
-   ./updater.sh ./apps/guestbook3/deployment.yaml "0.1" "0.2"
+   ./updater.sh ./helm-apps/helm-guestbook/deployment.yaml "0.1" "0.2"
+   ./updater.sh ./helm-apps/helm-guestbook2/deployment.yaml "0.1" "0.2"
+   ./updater.sh ./helm-apps/helm-guestbook3/deployment.yaml "0.1" "0.2"
 
    sleep 30
 
-   ./updater.sh ./apps/guestbook/deployment.yaml "0.2" "0.1"
-   ./updater.sh ./apps/guestbook2/deployment.yaml "0.2" "0.1"
-   ./updater.sh ./apps/guestbook3/deployment.yaml "0.2" "0.1"
+   ./updater.sh ./helm-apps/helm-guestbook/deployment.yaml "0.2" "0.1"
+   ./updater.sh ./helm-apps/helm-guestbook2/deployment.yaml "0.2" "0.1"
+   ./updater.sh ./helm-apps/helm-guestbook3/deployment.yaml "0.2" "0.1"
 
    sleep 30
 done
-
 
 
 
